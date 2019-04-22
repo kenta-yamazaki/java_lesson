2e26a0eac0cd5b84e263009b59b76c52c6519090
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-04-22 15:32:46 +0900

    list1-1のプログラムを作成しました

---
+++ src/sho1/Hello.java
@@ -0,0 +1,10 @@
+package sho1;
+/*
+    画面にHello!と表示して終了するプログラム（ファイル名はHello.java）
+ */
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello!");
+    }
+}

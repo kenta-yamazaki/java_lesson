89bc7d1312ca1fc273e538bc4dcaef72166d968e
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-04-23 11:10:07 +0900

    list2-3のプログラムを作成しました。

---
+++ src/sho2/Clac1a.java
@@ -0,0 +1,13 @@
+package sho2;
+
+/**
+ * printfメソッドの例
+ */
+public class Clac1a {
+    public static void main(String[] args) {
+        System.out.printf("加算の結果は%dです。%n", 3 + 2);
+        System.out.printf("減算の結果は%dです。%n", 5 - 8);
+        System.out.printf("乗算の結果は%dです。%n", 3 * 4);
+        System.out.printf("徐算の結果は%dです。%n", 7 / 3);
+    }
+}

0e34b0f0232e6784c0fb63ecce1605703161bde2
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-04-24 09:15:59 +0900

    問題3-1の実装漏れ修正

--- src/sho3/LightSpeed.java
+++ src/sho3/LightSpeed.java
@@ -1,4 +1,11 @@
 package sho3;
 
+/**
+ * 光の速度を秒速30万キロメートルとします。<br>
+ * このとき、光が1日に進む距離をJava言語で計算して求めてください。
+ */
 public class LightSpeed {
+    public static void main(String[] args) {
+        System.out.println("光が1日に進む距離は" + 300000L * 60 * 60 * 24 + "kmです。");
+    }
 }

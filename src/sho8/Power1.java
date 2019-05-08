0e4e18cb94cb54670fba1b7f4ec7c45e2ad66b37
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-08 16:18:09 +0900

    list8-4のプログラム作成しました。

---
+++ src/sho8/Power1.java
@@ -0,0 +1,19 @@
+package sho8;
+
+/**
+ * 引数が２つのメソッド
+ */
+public class Power1 {
+    public static void main(String[] args) {
+        System.out.println(getPower(8, 2));
+    }
+
+    /* xのn乗の計算 */
+    public static int getPower(int x, int n) {
+        int y = 1;
+        for (int i = 0; i < n; i++) {
+            y = y * x;
+        }
+        return y;
+    }
+}

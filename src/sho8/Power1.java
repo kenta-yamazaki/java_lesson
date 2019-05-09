270b5f7f8012b700788e5cffd89deb7e4d6cd2cc
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-09 11:57:51 +0900

    メソッドのコメント修正

--- src/sho8/Power1.java
+++ src/sho8/Power1.java
@@ -8,7 +8,13 @@ public class Power1 {
         System.out.println(getPower(8, 2));
     }
 
-    /* xのn乗の計算 */
+    /**
+     * xのn乗の計算
+     *
+     * @param x 累乗する数字
+     * @param n 累乗する回数
+     * @return 累乗されて出た結果
+     */
     public static int getPower(int x, int n) {
         int y = 1;
         for (int i = 0; i < n; i++) {

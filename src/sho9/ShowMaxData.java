93ec1bb55eac8a60f0db6810a775d6f8a6d72583
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-10 17:45:09 +0900

    変数名を変更

--- src/sho9/ShowMaxData.java
+++ src/sho9/ShowMaxData.java
@@ -5,11 +5,11 @@ package sho9;
  */
 public class ShowMaxData {
     public static void main(String[] args) {
-        int[] data = {31, 41, 59, 26, 53, 58, 97, 93, 23, 84};
-        int maxData = data[0];
-        for (int i = 0; i < data.length; i++) {
-            maxData = Math.max(maxData, data[i]);
+        int[] array = {31, 41, 59, 26, 53, 58, 97, 93, 23, 84};
+        int maxNumber = array[0];
+        for (int i = 0; i < array.length; i++) {
+            maxNumber = Math.max(maxNumber, array[i]);
         }
-        System.out.println("最大値は" + maxData + "です");
+        System.out.println("最大値は" + maxNumber + "です");
     }
 }

57f1de1f97e049daa8c3b4258596e3216ebc00d2
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-13 16:06:22 +0900

    JavaDoc形式に修正

--- src/sho10/Kamoku.java
+++ src/sho10/Kamoku.java
@@ -4,16 +4,27 @@ package sho10;
  * toStringを宣言する
  */
 public class Kamoku {
-    String kamokuName;  //科目名
-    int kamokuScore;    //点数
+    /** 科目名 */
+    String kamokuName;
+    /** 科目の点数 */
+    int kamokuScore;
 
-    //コンストラクタ
+    /**
+     * コンストラクタ
+     *
+     * @param kamokuName  科目名
+     * @param kamokuScore 科目の点数
+     */
     public Kamoku(String kamokuName, int kamokuScore) {
         this.kamokuName = kamokuName;
         this.kamokuScore = kamokuScore;
     }
 
-    //科目の文字列表現
+    /**
+     * 科目の文字列表現
+     *
+     * @return 戻り値
+     */
     @Override
     public String toString() {
         return this.kamokuName + "は" + kamokuScore + "点";

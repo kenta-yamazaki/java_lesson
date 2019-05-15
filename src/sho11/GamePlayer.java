f05250a38d6f809ddddac63acdca44c67322b9bd
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-15 11:48:44 +0900

    list11-12のプログラムを作成しました

---
+++ src/sho11/GamePlayer.java
@@ -0,0 +1,60 @@
+package sho11;
+
+/**
+ * list11-11
+ * [player:Mad Htter]<br>
+ * [player:March Hare]<br>
+ * [player:Alice]<br>
+ * と3人別々の名前が出ると思っていたのに、下記のように<br>
+ * 3人とも[player:Alice]という名前になっています。なぜでしょうか？
+ *
+ * <pre>{@code
+ * public class GamePlayer {
+ *     public static String playerName;
+ *
+ *     public GamePlayer(String name) {
+ *         playerName = name;
+ *     }
+ *
+ *     @Override
+ *     public String toString() {
+ *         return "[player:" + playerName + "]";
+ *     }
+ *
+ *     public static void main(String[] args) {
+ *         GamePlayer[] player = new GamePlayer[3];
+ *         player[0] = new GamePlayer("Mad Hatter");
+ *         player[1] = new GamePlayer("March Hare");
+ *         player[2] = new GamePlayer("Alice");
+ *         for (int i = 0; i < player.length; i++) {
+ *             System.out.println(player[i]);
+ *         }
+ *     }
+ * }
+ * }<pre/>
+ */
+public class GamePlayer {
+    public String playerName;
+
+    public GamePlayer(String name) {
+        playerName = name;
+    }
+
+    @Override
+    public String toString() {
+        return "[player:" + playerName + "]";
+    }
+
+    public static void main(String[] args) {
+        GamePlayer[] player = new GamePlayer[3];
+        player[0] = new GamePlayer("Mad Hatter");
+        player[1] = new GamePlayer("March Hare");
+        player[2] = new GamePlayer("Alice");
+        for (int i = 0; i < player.length; i++) {
+            System.out.println(player[i]);
+        }
+    }
+}
+/*
+ * playerNameはクラス変数で、他のインスタンスが作られる度にplayerNameが最新のものに上書きされてしまうため。
+ */

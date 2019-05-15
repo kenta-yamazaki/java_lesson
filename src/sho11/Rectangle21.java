0b2666d976a1d0c768c69e3319925388dfdae380
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-15 11:56:32 +0900

    問題11-6のプログラムを作成しました

---
+++ src/sho11/Rectangle21.java
@@ -0,0 +1,28 @@
+package sho11;
+
+/**
+ * list11-21 引数を１つにしてください
+ */
+public class Rectangle21 {
+    int width;
+    int height;
+
+    Rectangle21(int width, int height) {
+        setSize(width, height);
+    }
+
+    void setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean equals(Rectangle21 rectangle) {
+        if (rectangle == null) {
+            return false;
+        } else if (width == rectangle.width && height == rectangle.height) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

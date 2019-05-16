c51b394ecd912fdd407418859c5edc322c92917f
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-16 11:01:13 +0900

    list12-3のプログラム作成しました

---
+++ src/sho12/Rectangle3.java
@@ -0,0 +1,31 @@
+package sho12;
+
+/**
+ * list12-3 コンストラクタは継承されない
+ */
+public class Rectangle3 {
+    int width;
+    int height;
+
+    Rectangle3() {
+        this.width = 0;
+        this.height = 0;
+    }
+
+    Rectangle3(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
+
+class NamedRectangle3 extends Rectangle3 {
+    String name;
+
+    NamedRectangle3() {
+        name = "NO NAME";
+    }
+
+    NamedRectangle3(String name) {
+        this.name = name;
+    }
+}

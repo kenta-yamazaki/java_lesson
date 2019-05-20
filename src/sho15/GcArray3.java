495222f5c3d8da4661f1cf7d85898bc75550f374
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-20 15:53:46 +0900

    lista15-4のプログラム作成しました

---
+++ src/sho15/GcArray3.java
@@ -0,0 +1,20 @@
+package sho15;
+
+/**
+ * gcメソッドで強制的にガーベッジコレクションを起こす
+ */
+public class GcArray3 {
+    public static void main(String[] args) {
+        for (int i = 0; true; i++) {
+            int[] array = new int[1000];
+            for (int j = 0; j < array.length; j++) {
+                array[j] = j;
+            }
+            if (i % 100 == 0) {
+                System.out.println("gcを呼び出します。");
+                System.gc();
+            }
+            System.out.println("残りのメモリ = " + Runtime.getRuntime().freeMemory());
+        }
+    }
+}

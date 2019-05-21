74bdf4d95e47494e7573421163757624a9e425af
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-21 14:11:06 +0900

    三項演算子、例外を修正しました

--- src/sho13/FactorialTest.java
+++ src/sho13/FactorialTest.java
@@ -19,19 +19,15 @@ public class FactorialTest {
     public static void main(String[] args) {
         try {
             System.out.println(factorial(10));
-        } catch (Exception e) {
+        } catch (IllegalArgumentException e) {
             System.out.println(e);
         }
     }
 
-    public static int factorial(int n) throws Exception {
-        if (n > 0) {
-            return n * factorial(n - 1);
-        } else if (n == 0) {
-            return 1;
-        } else {
-            throw new Exception("負の値は指定できません。");
+    public static int factorial(int n) throws IllegalArgumentException {
+        if (n < 0) {
+            throw new IllegalArgumentException("負の値は指定できません。");
         }
-
+        return n > 0 ? n * factorial(n - 1) : 1;
     }
 }

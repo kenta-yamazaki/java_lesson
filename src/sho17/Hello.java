2d4f45140800173c377c3a1dd94f780689d07f4a
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-27 15:51:12 +0900

    list17-10のプログラム作成しました。

---
+++ src/sho17/Hello.java
@@ -0,0 +1,12 @@
+package sho17;
+
+import static java.lang.System.out;
+
+/**
+ * java.lang.System.out をスタティックにimportした例
+ */
+public class Hello {
+    public static void main(String[] args) {
+        out.println("Hello!");
+    }
+}

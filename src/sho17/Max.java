51b3c906e453db6cc4348b82c1b1970cf38cefeb
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-27 15:51:33 +0900

    list17-11のプログラム作成しました。

---
+++ src/sho17/Max.java
@@ -0,0 +1,16 @@
+package sho17;
+
+import static java.lang.System.out;
+import static java.lang.Math.max;
+
+/**
+ * java.lang.Math.maxをスタティックにimportした例
+ */
+public class Max {
+    public static void main(String[] args){
+        int x = 45;
+        int y = 100;
+        int z = max(x,y);
+        out.println("z = " + z);
+    }
+}

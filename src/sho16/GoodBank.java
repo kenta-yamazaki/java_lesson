b49148ffefa89a3c4eb52d82954e798a7e67923e
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-27 10:59:48 +0900

    list16-5,6のプログラム作成しました。

---
+++ src/sho16/GoodBank.java
@@ -0,0 +1,19 @@
+package sho16;
+
+/**
+ * list16-5 良い銀行
+ */
+public class GoodBank {
+    private int value = 0;
+
+    public synchronized void addMoney(int money) {
+        int currentValue = value;
+        System.out.println(Thread.currentThread() + "がaddMoneyに入りました。");
+        value += money;
+        if (currentValue + money != value) {
+            System.out.println(Thread.currentThread() + "で矛盾が発生しました。");
+            System.exit(-1);
+        }
+        System.out.println(Thread.currentThread() + "がaddMoneyから出ました。");
+    }
+}

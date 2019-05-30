7d60cd9e22b5e91c1f8d8fd1cf3e5e83092254c4
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-05-30 10:32:45 +0900

    list19-9のプログラム作成しました。

---
+++ src/sho19/LinkedListTest1.java
@@ -0,0 +1,23 @@
+package sho19;
+
+import java.util.LinkedList;
+
+/**
+ * list19-9 LinkedListを使ってAliceを先頭に挿入するサンプル
+ */
+public class LinkedListTest1 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("Bob");
+        list.add("Chris");
+        list.add("Diane");
+        list.add("Elmo");
+
+        System.out.println(list);
+
+        list.addFirst("Alice");
+
+        System.out.println(list);
+    }
+}
